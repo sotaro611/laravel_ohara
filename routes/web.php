5c6df1c99fbc19ff5cbe5b324a55e82fec28bf73
Suzuki <M2420017@ngo.ohara.ac.jp>
2025-07-01 12:30:33 +0900

    0701 index,create

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,49 @@
 <?php
 
+// use文。このファイル内で扱うクラスをインポートするための機能
+use App\Http\Controllers\ContactFormController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
+// お問い合わせフォーム用ルート
+//Route::get('/contacts', [ContactFormController::class, 'index'])->name('contacts.index');
+//Route::get('/contacts', [ContactFormController::class, 'index'])->name('contacts.index');
+//　リソースコントローラー用のまとめてルート作成
+//Route::resource('contacts',ContactFormController::class);
+Route::prefix('contacts') // 頭に contacts をつける ​
+
+    ->middleware(['auth']) // 認証 ​
+
+    ->name('contacts.') // ルート名 ​
+
+    ->controller(ContactFormController::class) // コントローラ指定 ​
+
+    ->group(
+        function () { // グループ化 ​
+
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+        }
+    );
+
+
+// テスト用ルーティング
+// ルートには名前を付けることができる。名前を付けておくと、ビューからリンクを貼るときに便利
+Route::get('tests/test', [TestController::class, 'index'])->name('test.index');
+
+
+
+
+// ルーティングの定義 Route: 通信方式(post or get)(アドレス,処理);
+// ex. [/]というURLにアクセスしたら return view('welcome')という処理を行う。
+// view(ビューファイル)でビューファイルを表示。自動的にresoueces/viewの中身を参照し、welcome.blade.phpのファイル名のみで指定できる。
 Route::get('/', function () {
     return view('welcome');
 });
 
+// middleware(auth)⇒認証機能。ログインしていないユーザーでなければdashboardにはアクセスできない。
+// ログインしていないユーザーがアクセスするとログイン画面にリダイレクトがかかる。
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +54,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
